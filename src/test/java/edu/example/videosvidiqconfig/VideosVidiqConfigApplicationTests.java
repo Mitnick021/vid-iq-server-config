bd1f578e1cf6a79c6ae78d7b15b408b2350af632
Sergio Manuelle Pinta De La Cruz <sergio.pinta2016123@gmail.com>
2023-12-13 20:47:52 -0500

    Carga inicial

---
+++ src/test/java/edu/example/videosvidiqconfig/VideosVidiqConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.example.videosvidiqconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideosVidiqConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
